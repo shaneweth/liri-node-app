e32711c9376a23abd22f43d150a5df2288b7218e
willow <shaneweth@gmail.com>
2019-04-15 16:15:54 -0400

    Spotify is working. Need to create search for the other 2 items.

--- liri.js
+++ liri.js
@@ -1,17 +1,11 @@
 require("dotenv").config();
-
 const keys = require("./keys.js");
-
 const Spotify = require("node-spotify-api");
 
-const spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-});
-
+const spotify = new Spotify(keys.spotify);
 const spotifyThisSong = process.argv[2];
-const mySong = process.argv[3];
 
+mySong = process.argv.splice(3).join(" ");
 
 
 if (spotifyThisSong === "spotify-this-song" && mySong) {
@@ -21,41 +15,44 @@ if (spotifyThisSong === "spotify-this-song" && mySong) {
     theSign();
 }
 
-
-
 function getSong() {
 
-    // const tracks = []
-    
     spotify
-        .search({ type: "track", query: mySong, limit: 5 })
-        .then((...response) => {
-            
-                response = response.map(results => results.tracks.items);
-                
-                console.log(response);
+        .search({ type: "track", query: mySong, available_markets: "US", limit: 5 })
+        .then((response) => {
 
-                
+            for (let i = 0; i < response.tracks.items.length; i++) {
 
-            })
+                let nameRes =   response.tracks.items[i].name;
+                let albumRes =  response.tracks.items[i].album.name;
+                let artistRes = response.tracks.items[i].album.artists[0].name;
+                let urlRes =    response.tracks.items[i].preview_url;
+
+
+                console.log("<================================>");
+                console.log("  ");
+                console.log("Song Name: " + nameRes);
+                console.log("Album Name: " + albumRes);
+                console.log("Artist Name: " + artistRes);
+                console.log("Preview Link: " + urlRes);
+                console.log("  ");
+                console.log("<________________________________>");
+
+            }
+
+        })
         .catch(function (err) {
             console.log(err);
         });
 
-    }
+}
 
+function theSign() {
+    spotify.search({ type: "track", query: "The Sign" }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
 
-
-
-
-
-
-// function theSign() {
-//     spotify.search({ type: "track", query: "The Sign" }, function (err, data) {
-//         if (err) {
-//             return console.log("Error occurred: " + err);
-//         }
-
-//         console.log(JSON.parse(data, null, 2));
-//     });
-// }
+        console.log(JSON.parse(data, null, 2));
+    });
+}
